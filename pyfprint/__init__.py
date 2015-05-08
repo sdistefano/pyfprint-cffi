be6b10f5429fa0491f7387caed3d49d303dae519
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-05-08 16:07:56 +0200

    export constants

--- pyfprint/__init__.py
+++ pyfprint/__init__.py
@@ -11,6 +11,20 @@ __all__ = [
     "Fprint",
     "Image",
     "identify",
+
+    "ENROLL_COMPLETE",
+    "ENROLL_FAIL",
+    "ENROLL_PASS",
+    "ENROLL_RETRY",
+    "ENROLL_RETRY_TOO_SHORT",
+    "ENROLL_RETRY_CENTER_FINGER",
+    "ENROLL_RETRY_REMOVE_FINGER",
+    "VERIFY_NO_MATCH",
+    "VERIFY_MATCH",
+    "VERIFY_RETRY",
+    "VERIFY_RETRY_TOO_SHORT",
+    "VERIFY_RETRY_CENTER_FINGER",
+    "VERIFY_RETRY_REMOVE_FINGER",
 ]
 
 from .pyfprint import *
